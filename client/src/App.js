7bc400b15cc3f1f8ea8e86ad3f328206b3ca306d
joelgirouxx <j.giroux9@gmail.com>
2021-05-11 13:33:35 -0400

    buttons rendered

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import Genres from "./Genres.jsx";
+import GamesHome from "./GamesHome.jsx";
 import "./App.css";
+import { Route } from "react-router";
 
 const API_KEY = process.env.REACT_APP_AIRTABLE_API_KEY;
 const url = "https://api.airtable.com/v0/appWBDWQi6vOuApxc/Table%201?api_key=";
 
 function App() {
-  const [gameList, setGameList] = useState({});
+  const [gameList, setGameList] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const resp = await axios.get(url + API_KEY);
-      return resp.data.records;
+      const gameArr = resp.data.records;
+      setGameList(gameArr);
     };
-    setGameList(fetchData);
+    fetchData();
   }, []);
 
-  return <div className="App"></div>;
+  console.log(gameList);
+
+  return (
+    <div>
+      <Genres games={gameList} />
+      <Route exact path="/">
+        <GamesHome games={gameList} />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
--- client/src/Genres.jsx
+++ client/src/Genres.jsx
@@ -1,17 +1,28 @@
 function Genres(props) {
+  const compareCatagories = (catagory) => {
+    const filteredGames = props.games.filter((game) => {
+      return !game.fields.genres.find((cat) => cat == catagory);
+    });
+    return filteredGames;
+  };
+
   return (
     <div>
-      <button>Adventure</button>
-      <button>Action</button>
-      <button>RPG</button>
-      <button>FPS</button>
-      <button>TPS</button>
-      <button>Battle Royale</button>
-      <button>Simulation</button>
-      <button>Social</button>
-      <button>Fighter</button>
-      <button>Sandbox</button>
-      <button>Platformer</button>
+      <button onClick={compareCatagories("Adventure")}>Adventure</button>
+      <button onClick={compareCatagories("Action")}>Action</button>
+      <button onClick={compareCatagories("RPG")}>RPG</button>
+      <button onClick={compareCatagories("FPS")}>FPS</button>
+      <button onClick={compareCatagories("TPS")}>TPS</button>
+      <button onClick={compareCatagories("Battle Royale")}>
+        Battle Royale
+      </button>
+      <button onClick={compareCatagories("Simulation")}>Simulation</button>
+      <button onClick={compareCatagories("Social")}>Social</button>
+      <button onClick={compareCatagories("Fighter")}>Fighter</button>
+      <button onClick={compareCatagories("Sandbox")}>Sandbox</button>
+      <button onClick={compareCatagories("Platform games")}>Platformer</button>
     </div>
   );
 }
+
+export default Genres;
