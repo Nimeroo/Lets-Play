0b53828f8eda411aee02e806e4ddc99721d31b27
joelgirouxx <j.giroux9@gmail.com>
2021-05-12 10:45:00 -0400

    Genre filtered array created

---
+++ client/src/Components/Genres.jsx
@@ -0,0 +1,39 @@
+import { useParams, Link } from "react-router-dom";
+
+function Genres(props) {
+  const compareCatagories = (catagory) => {
+    const filteredGames = props.games.filter((game) => {
+      return game.fields.genres.includes(catagory);
+    });
+    return filteredGames;
+  };
+
+  console.log(compareCatagories("Adventure"));
+  return (
+    <div>
+      <Link to="/">
+        <button onClick={() => compareCatagories("Adventure")}>
+          Adventure
+        </button>
+      </Link>
+      <button onClick={() => compareCatagories("Action")}>Action</button>
+      <button onClick={() => compareCatagories("RPG")}>RPG</button>
+      <button onClick={() => compareCatagories("FPS")}>FPS</button>
+      <button onClick={() => compareCatagories("TPS")}>TPS</button>
+      <button onClick={() => compareCatagories("Battle Royale")}>
+        Battle Royale
+      </button>
+      <button onClick={() => compareCatagories("Simulation")}>
+        Simulation
+      </button>
+      <button onClick={() => compareCatagories("Social")}>Social</button>
+      <button onClick={() => compareCatagories("Fighter")}>Fighter</button>
+      <button onClick={() => compareCatagories("Sandbox")}>Sandbox</button>
+      <button onClick={() => compareCatagories("Platform games")}>
+        Platformer
+      </button>
+    </div>
+  );
+}
+
+export default Genres;
