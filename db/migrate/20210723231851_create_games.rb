84c43ec5b5a5756ff872f1ebc4952d7de81ac6fe
Nimeroo <girou.joe@gmail.com>
2021-07-25 23:21:17 -0400

    user authentication added and bcrypt/jwt gem installed

--- db/migrate/20210723231851_create_games.rb
+++ db/migrate/20210723231851_create_games.rb
@@ -3,7 +3,7 @@ class CreateGames < ActiveRecord::Migration[6.1]
     create_table :games do |t|
       t.string :title
       t.string :thumbnail
-      t.string :screenshot array: true, default: []
+      t.text :screenshot, array: true, default: []
       t.string :summary
       t.integer :rating
       t.references :user, null: false, foreign_key: true
