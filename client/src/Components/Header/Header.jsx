1903fd6f6b0ef431ad7fabc264e77ed9101f7768
Nimeroo <girou.joe@gmail.com>
2021-08-11 05:53:37 -0400

    Authenticated and unauthenticated nav options added

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,8 +1,36 @@
-function Header(props) {
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+
+function Header({ user, logout }) {
+  const authenticatedOptions = (
+    <>
+      <button onClick={logout} className="links" id="logout">
+        Logout
+      </button>
+    </>
+  );
+
+  const unauthenticatedOptions = (
+    <div className="user-links">
+      <NavLink className="links" to="/signup">
+        Sign Up
+      </NavLink>
+      <NavLink className="links" to="/login">
+        Login
+      </NavLink>
+    </div>
+  );
+
   return (
-    <header>
-      <h1>Lets-Play!</h1>
-    </header>
+    <div id="nav">
+      <NavLink to="/">
+        <img id="home-icon" src="../../assets/Lets-Play-Logo.png"></img>
+      </NavLink>
+      <div className="nav-user-info">
+        {user ? authenticatedOptions : unauthenticatedOptions}
+        {user && <div>Welcome bak {user.username}!</div>}
+      </div>
+    </div>
   );
 }
 
