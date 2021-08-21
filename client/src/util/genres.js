7bdf6f3fa4d93b5887d8a9ddac47e8b998666a36
Nimeroo <girou.joe@gmail.com>
2021-08-20 21:03:45 -0400

    genre list file created

---
+++ client/src/util/genres.js
@@ -0,0 +1,18 @@
+import { Route } from "react-router-dom";
+
+const genres =  [
+        "Adventure",
+        "Action",
+        "RPG",
+        "FPS",
+        "TPS",
+        "Battle Royale",
+        "Simulation",
+        "Social",
+        "Fighter",
+        "Sandbox",
+        "Platformer",
+    ]
+
+
+export default genres;
